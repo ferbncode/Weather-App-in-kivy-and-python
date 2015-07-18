dc7021d5060a94ff5c65d6024e341d8f02a82af4
ferbncode <suyashgargsfam@gmail.com>
2015-07-18 23:27:04 +0530

    Added Coverpage and add loaction button in current weather page

--- main.py
+++ main.py
@@ -8,13 +8,25 @@ from kivy.network.urlrequest import UrlRequest
 
 import json
 
+from kivy.uix.listview import ListItemButton
+
+from kivy.factory import Factory
+
 class WeatherApp(App):
 	pass
 class WeatherRoot(BoxLayout):
 	'''this is the root widget which must have various configuration methods
 	to decide how the screen looks.Other classes declared below will be children
 	of the root class WeatherRoot in the kivy language file '''
-	pass
+	def show_current_weather(self,location):
+		self.clear_widgets()
+		current_weather=Factory.CurrentLocation()
+		current_weather.location=location
+		self.add_widget(current_weather)
+	def addlocation(self):
+		self.clear_widgets()
+		self.add_widget(AddLocationForm())
+
 class AddLocationForm(BoxLayout):
 	'''this class inherits from the BoxLayout widget of the kivy language
 	and contains the logic related to the location form,however the design part 
@@ -26,21 +38,37 @@ class AddLocationForm(BoxLayout):
 		print(self.search_input.text)
 		search_template="http://api.openweathermap.org/data/2.5/" + "find?q={}&type=like"
 		search_url=search_template.format(self.search_input.text)
-		print("human2")
+		print("finding location")
 		request=UrlRequest(search_url,self.found_location)
 	def found_location(self,request,data):
 		'''To notice in this method is the ListView can use different classes
 		as the widget to be displayed .Two classes ListItemLabel and ListItemButton
 		that behave normally only that they are containing info for tracking selection
-		.Thus,its often a good idea to extend these classes'''
+		.Thus,its often a good idea to extend these classes
+		{"employees":[
+    	{"firstName":"John", "lastName":"Doe"},
+    	{"firstName":"Anna", "lastName":"Smith"},
+    	{"firstName":"Peter", "lastName":"Jones"}
+		]}
+		Above is an example of the json code and so is to be taken care of.
+		json is lightweight data interchange format.It is self-describing 
+		.It is a syntax for storing and exchanging data.Its an alternative of 
+		XML.Again,XML is a markup language made only to describe data.HTML to
+		display data.
+		'''
 		data=json.loads(data.decode()) if not isinstance(data,dict) else data
 		cities = ["{} ({})".format(d['name'], d['sys']['country']) for d in data['list']]
 		self.search_results.item_strings=cities
 		self.search_results.adapter.data.clear()
 		self.search_results.adapter.data.extend(cities)
 		self.search_results._trigger_reset_populate()
-		print("human")
+		print("function found_location searched")
 		#this above line is just for the update the display when data changes
+class LocationButton(ListItemButton):
+	pass
+
+class CoverPage(BoxLayout):
+	pass
 
 if __name__=='__main__':
 	WeatherApp().run()
